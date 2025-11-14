a5b3d536be55ab059857470fadc057d515f6e14f
Djibrani Yuda <djibyuda@gmail.com>
2025-11-15 00:43:11 +0700

    fixing railway issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,12 @@
 import express from "express";
-// import mongoose from "mongoose";
 import { env } from "./lib/env.js";
-import path from "path";
-import { fileURLToPath } from "url";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
 import { serve } from "inngest/express";
 import { inngest, inngestFunctions } from "./lib/inngest.js";
 
 const app = express();
-
-const __dirname = fileURLToPath(import.meta.url);
-
 const PORT = env.PORT;
-
-app.get("/api/v1/health", (req, res) => {
-  res.status(200).json({ message: "Server is running!" });
-});
 
 app.use(express.json());
 app.use(
@@ -26,6 +16,10 @@ app.use(
   })
 );
 
+app.get("/api/v1/health", (req, res) => {
+  res.status(200).json({ message: "Server is running!" });
+});
+
 app.use(
   "/api/inngest",
   serve({
@@ -33,15 +27,6 @@ app.use(
     functions: inngestFunctions,
   })
 );
-
-// make ready for deployment
-if (env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
-}
 
 const startServer = async () => {
   try {
